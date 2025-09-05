1473150bf069cfe59826d81d62441a80879cc32b
SamaraDcruz <samaragracedcruz@gmail.com>
2025-09-05 09:44:16 -0400

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,11 @@
+print("Enter a nummber:")
+
+num = int(input())
+
+while True:
+    if num % 2 == 0:
+        while True:
+            print("Bye Bye")
+    else:
+        print("Not divisible by 2")
+        break
